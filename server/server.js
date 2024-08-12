53fd68e1f9a3259b94b36b03769b4f151d9602b1
seonyeonghun2 <kjca1117>
2024-08-12 11:25:04 +0900

    add module & update codes

--- server/server.js
+++ server/server.js
@@ -1,49 +1,55 @@
 import express from "express";
-import mariadb from "mariadb"
+import mariadb from "mariadb";
+import morgan from "morgan";
+import colors from "colors";
 import dotenv from "dotenv";
 dotenv.config();
 
-const app = express()
+const app = express();
 
+app.use(morgan("dev"));
 // route : .get(): 받기, .post(): 보내기, .put(): 보내서 부분 수정, .delete() : 보내서 삭제
 // RESTful API : REpresentational (대표성 있는 방식으로 요청 URL을 생성하는 규칙)
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World</h1>");
+});
 
 // db connection
 const pool = mariadb.createPool({
-  host: process.env.DB_HOST, 
-  user: process.env.DB_USER, 
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
   password: process.env.DB_PWD,
-  connectionLimit: 5
+  database: process.env.DB_NAME,
+  connectionLimit: 5,
+  trace: true,
 });
 
-app.get('/getAllUsers', function (req, res) {
-  pool.getConnection()
-    .then(conn => {
-    
-      conn.query("SELECT 1 as val")
+app.get("/getAllUsers", function (req, res) {
+  pool
+    .getConnection()
+    .then((conn) => {
+      console.log(`MariaDB connected: ${conn.threadId}`.bgYellow.white);
+      conn
+        .query("SELECT * FROM students")
         .then((rows) => {
-          console.log(rows); //[ {val: 1}, meta: ... ]
-          //Table must have been created before 
-          // " CREATE TABLE myTable (id int, val varchar(255)) "
+          //console.log(rows); //[ {val: 1}, meta: ... ]
+          res.status(200).json(rows);
           return rows;
         })
         .then((res) => {
           console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
           conn.end();
         })
-        .catch(err => {
+        .catch((err) => {
           //handle error
-          console.log(err); 
+          console.log(err);
           conn.end();
-        })
-        
-    }).catch(err => {
+        });
+    })
+    .catch((err) => {
       //not connected
     });
-  res.send('Hello World')
-})
+  // res.send("=========== db connection succeed ==========");
+});
 
-app.listen(3000)
+app.listen(3000);
